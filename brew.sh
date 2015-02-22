5d644b4f508dfa14c5386c2562fceb05c5ab48d3
Janne Sinivirta <janne.sinivirta@nitorcreations.com>
2015-02-22 08:34:21 +0200

    initial version of brew calls

---
+++ brew.sh
@@ -0,0 +1,32 @@
+# make sure you have installed xcode and accepted it's license already
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+# brew installs
+brew install zsh
+brew install git
+brew install node
+
+
+
+# brew casks
+
+brew install caskroom/cask/brew-cask
+export HOMEBREW_CASK_OPTS="--appdir=/Applications
+
+sudo -v
+
+brew cask install java
+brew cask install caskroom/versions/java7
+brew cask install google-chrome
+brew cask install alfred
+brew cask install vlc
+brew cask install firefox
+brew cask install dropbox
+brew cask install skype
+brew cask install sublime-text
+brew cask install spotify
+brew cask install caffeine
+
