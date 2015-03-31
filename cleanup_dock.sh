f00217172c230891d72c9ce5eb78ccf440eaacb9
Janne Sinivirta <janne.sinivirta@nitorcreations.com>
2015-03-31 08:50:54 +0300

    cleanup_dock.sh adds google-chrome to dock

--- cleanup_dock.sh
+++ cleanup_dock.sh
@@ -1,3 +1,4 @@
+dockutil --add file:///opt/homebrew-cask/Caskroom/google-chrome/latest/Google%20Chrome.app/ --after Launchpad
 dockutil --remove Mail
 dockutil --remove Contacts
 dockutil --remove Maps
