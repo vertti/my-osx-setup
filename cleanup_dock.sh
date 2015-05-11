a07e3c4ff3e312937dfb0830133394cbb175f2b8
Janne Sinivirta <janne.sinivirta@nitorcreations.com>
2015-05-11 19:09:15 +0300

    more removals to cleanup_dock.sh

--- cleanup_dock.sh
+++ cleanup_dock.sh
@@ -1,8 +1,14 @@
 dockutil --add file:///opt/homebrew-cask/Caskroom/google-chrome/latest/Google%20Chrome.app/ --after Launchpad
+dockutil --remove Safari
 dockutil --remove Mail
 dockutil --remove Contacts
 dockutil --remove Maps
 dockutil --remove iBooks
 dockutil --remove iTunes
+dockutil --remove FaceTime
+dockutil --remove Messages
 dockutil --remove "Microsoft PowerPoint"
+dockutil --remove "Microsoft Lync"
+dockutil --remove "Microsoft Outlook"
+dockutil --remove "Microsoft Document Connection"
 
